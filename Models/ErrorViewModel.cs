482243ad2a270267c38854baf5095bf8112c2727
RodrigoLeivaDiaz <leivadiazrodrigo@gmail.com>
2023-04-23 21:40:12 -0300

    Branch base para comenzar

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RecepcionMedica.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
